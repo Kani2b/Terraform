a00d23251f1fea0ae1e314b0a12b1872212e1b09
Kani2b <kanithan@live.com>
2023-10-16 14:31:45 +0000

    vpc_test

---
+++ vpc.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+  region = "eu-north-1"
+  profile = "default"
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-0989fb15ce71ba39e"
+    instance_type = "t3.micro"
+    key_name = "terra-key"
+   //vpc_security_group_ids = ["${aws_security_group.rtp03-sg.id}"]
+   subnet_id = "${aws_subnet.rtp03-public_subent_01.id}"
+}
+
+
+
+resource "aws_security_group" "rtp03-sg" {
+    name = "rtp03-sg"
+    vpc_id = "${aws_vpc.rtp03-vpc.id}"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+   
+
+ ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ssh-sg"
+
+    }
+
+}
+
+//creating a VPC
+resource "aws_vpc" "rtp03-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+      Name = "rpt03-vpc"
+    }
+  
+}
+
+
+// Creating a Subnet 
+resource "aws_subnet" "rtp03-public_subent_01" {
+    vpc_id = "${aws_vpc.rtp03-vpc.id}"
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "eu-north-1a"
+    tags = {
+      Name = "rtp03-public_subent_01"
+    }
+  
+}
+
+//Creating a Internet Gateway 
+resource "aws_internet_gateway" "rtp03-igw" {
+    vpc_id = "${aws_vpc.rtp03-vpc.id}"
+    tags = {
+      Name = "rtp03-igw"
+    }
+}
+
+// Create a route table 
+resource "aws_route_table" "rtp03-public-rt" {
+    vpc_id = "${aws_vpc.rtp03-vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.rtp03-igw.id}"
+    }
+    tags = {
+      Name = "rtp03-public-rt"
+    }
+}
+
+// Associate subnet with routetable 
+
+resource "aws_route_table_association" "rtp03-rta-public-subent-1" {
+    subnet_id = "${aws_subnet.rtp03-public_subent_01.id}"
+    route_table_id = "${aws_route_table.rtp03-public-rt.id}"
+  
+}
+
